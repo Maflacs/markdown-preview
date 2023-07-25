2d91c5095f5176a680cc06b0607e26bf1f36e842
Maflacs <effa87@gmail.com>
2023-07-25 15:46:55 +0200

    Markdown previewer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { marked } from 'marked';
+
+
 
 function App() {
+  const [inputText, setInputText] = useState(`# Heading (H1 size)
+  ## Subheading (H2 size)
+  [Link](https://www.rd.com/wp-content/uploads/2023/04/Hilarious-Cat-Memes-6.jpg?resize=700,700)
+
+  \`Inline code\`
+  \`\`\`
+  // Code block
+  const example = "Hello World";
+  console.log(example);
+  \`\`\`
+  - List item 1
+  - List item 2
+
+  > Blockquote
+
+  ![Image](https://www.rd.com/wp-content/uploads/2023/04/Hilarious-Cat-Memes-6.jpg?resize=100,100)
+
+  **Bold text**`);
+
+  const handleInputChange = (event) => {
+    setInputText(event.target.value);
+  }
+
+  const getMarkdownText = () => {
+    return { __html: marked(inputText) };
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div id="app">
+
+        <div className="markdown">
+          <label htmlFor="editor" className="title">Editor</label>
+          <textarea
+            className="text"
+            id="editor"
+            type="text"
+            name="editor"
+            rows="10" cols="50"
+            value={inputText}
+            onChange={handleInputChange}>
+
+          </textarea>
+        </div>
+      </div>
+      <div className="previewer">
+        <div className="title">Preview</div>
+        <div className="text" id="preview" dangerouslySetInnerHTML={getMarkdownText()}>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
